69d3a84f956686e26eb37840af6318589c99e686
GrieVeR-13 <GrieVeR-C@list.ru>
2021-10-16 23:01:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://android.googlesource.com/platform/external/e2fsprogs/+/refs/heads/master/lib/uuid/
---
+++ config.h
@@ -0,0 +1,6 @@
+//https://android.googlesource.com/platform/external/e2fsprogs/+/refs/heads/master/lib/uuid/
+
+#ifndef EDS3_UUID_CONFIG_H
+#define EDS3_UUID_CONFIG_H
+
+#endif //EDS3_UUID_CONFIG_H
